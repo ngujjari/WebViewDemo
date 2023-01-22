ee9a75948aea29a160346516b7eda5079262a6d5
ngujjari <ngujjari@hanover.com>
2023-01-22 00:01:32 -0500

    IOS webviewallow  external link open

--- UnifiDemoAndroid/app/src/main/java/com/syf/unifidemoandroid/SyfPageActivity.kt
+++ UnifiDemoAndroid/app/src/main/java/com/syf/unifidemoandroid/SyfPageActivity.kt
@@ -13,7 +13,7 @@ class SyfPageActivity : AppCompatActivity() {
     private val TAG = "SyfPageActivity"
     // private val url = "https://ppdpone.syfpos.com/mit/?widgetload=y&amount=100&flowType=pdp&partnerId=PI53421676&calcLoadTime=N";
    // private val url = "https://ppdpone.syfpos.com/mpp/mpp?partnerId=PI53421676&amount=800&syfToken=MPP16706334026627PI53421676&productCategoryNames=&flowType=PDP&cid=unifitest&adobe_mc=MCMID%3D11699604732310737292115892261680123387%7CMCORGID%3D22602B6956FAB4777F000101%2540AdobeOrg%7CTS%3D1670633403&_ga=2.172105827.1545651286.1670631858-1876618331.1669861855"
-    private val url = "http://192.168.86.25"
+    private val url = "http://192.168.86.23"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +39,8 @@ class SyfPageActivity : AppCompatActivity() {
         override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
             Log.v(TAG, "shouldOverrideUrlLoading url=" + url);
             return if (url != null && (url.startsWith("https://dpdpone.syfpos.com/cs/")
-                        || url.startsWith("https://www.synchrony.com/"))) {
+                        || url.startsWith("https://www.synchrony.com/")
+                        || url.startsWith("https://qbuy.syf.com/"))) {
                 view.context.startActivity(
                     Intent(Intent.ACTION_VIEW, Uri.parse(url))
                 )
@@ -52,7 +53,7 @@ class SyfPageActivity : AppCompatActivity() {
            // showProgress(true)
             Log.v(TAG, "onPageStarted url=" + url);
             var jsonPayloadScript = "var jsonObj = {syfPartnerId:\"PI53421676\"," +
-                    "tokenId:\"1850ed2d835PI5342167614545\"," +
+                    "tokenId:\"185cd468fd4PI5342167627013\"," +
                     "encryptKey:\"\",modalType:\"\",childMid:\"\",childPcgc:\"\",childTransType:\"\",pcgc:\"\",partnerCode:\"\",clientToken:\"\",postbackid:\"d979e5b7-6382-4e4e-b269-aab027bbed58\",clientTransId:\"\",cardNumber:\"\",custFirstName:\"\",custLastName:\"\",expMonth:\"\",expYear:\"\",custZipCode:\"\",custAddress1:\"\",phoneNumb:\"\",appartment:\"\",emailAddr:\"\",custCity:\"\",upeProgramName:\"\",custState:\"\",transPromo1:\"\",iniPurAmt:\"\",mid:\"\",productCategoryNames:\"\",transAmount1:\"700\",transAmounts:\"\",initialAmount:\"\",envUrl:\"https://dpdpone.syfpos.com/mitservice/\",productAttributes:\"\",processInd:\"3\"}"
             view.evaluateJavascript(jsonPayloadScript, null)
             super.onPageStarted(view, url, favicon)
--- UnifiIntegratonApp/UnifiIntegratonApp/AdvWebviewViewController.swift
+++ UnifiIntegratonApp/UnifiIntegratonApp/AdvWebviewViewController.swift
@@ -8,7 +8,7 @@
 import WebKit
 import UIKit
 
-class AdvWebviewViewController: UIViewController, WKScriptMessageHandler {
+class AdvWebviewViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
   
     // https://www.innominds.com/blog/wkwebview-to-native-code-interaction
     
@@ -33,7 +33,7 @@ class AdvWebviewViewController: UIViewController, WKScriptMessageHandler {
         let contentController = WKUserContentController()
        
        // let scriptSource = "document.body.style.backgroundColor = `Aqua`;"
-        let scriptSource = "var jsonObj = {syfPartnerId:\"PI53421676\",tokenId:\"185089a155bPI5342167644931\",encryptKey:\"\",modalType:\"\",childMid:\"\",childPcgc:\"\",childTransType:\"\",pcgc:\"\",partnerCode:\"\",clientToken:\"\",postbackid:\"d979e5b7-6382-4e4e-b269-aab027bbed58\",clientTransId:\"\",cardNumber:\"\",custFirstName:\"\",custLastName:\"\",expMonth:\"\",expYear:\"\",custZipCode:\"\",custAddress1:\"\",phoneNumb:\"\",appartment:\"\",emailAddr:\"\",custCity:\"\",upeProgramName:\"\",custState:\"\",transPromo1:\"\",iniPurAmt:\"\",mid:\"\",productCategoryNames:\"\",transAmount1:\"700\",transAmounts:\"\",initialAmount:\"\",envUrl:\"https://dpdpone.syfpos.com/mitservice/\",productAttributes:\"\",processInd:\"3\"};"
+        let scriptSource = "var jsonObj = {syfPartnerId:\"PI53421676\",tokenId:\"PI5342167618061185d7d33a46\",encryptKey:\"\",modalType:\"\",childMid:\"\",childPcgc:\"\",childTransType:\"\",pcgc:\"\",partnerCode:\"\",clientToken:\"\",postbackid:\"d979e5b7-6382-4e4e-b269-aab027bbed58\",clientTransId:\"\",cardNumber:\"\",custFirstName:\"\",custLastName:\"\",expMonth:\"\",expYear:\"\",custZipCode:\"\",custAddress1:\"\",phoneNumb:\"\",appartment:\"\",emailAddr:\"\",custCity:\"\",upeProgramName:\"\",custState:\"\",transPromo1:\"\",iniPurAmt:\"\",mid:\"\",productCategoryNames:\"\",transAmount1:\"700\",transAmounts:\"\",initialAmount:\"\",envUrl:\"https://dpdpone.syfpos.com/mitservice/\",productAttributes:\"\",processInd:\"3\"};"
         
        // let scriptSource = "document.body.style.backgroundColor = `white`;"
         let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
@@ -49,15 +49,67 @@ class AdvWebviewViewController: UIViewController, WKScriptMessageHandler {
         return webView
     }()
     
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print("Webview redirect .....")
+           if navigationAction.navigationType == .linkActivated  {
+                if let url = navigationAction.request.url,
+                    let host = url.host, !host.hasPrefix("https://dpdpone.syfpos.com"), // Webview target URL Prefix. All others will open in safari.
+                    UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url)
+                    print(url)
+                    print("Redirected to browser. No need to open it locally")
+                    decisionHandler(.cancel)
+                    return
+                } else {
+                    print("Open it locally")
+                    decisionHandler(.allow)
+                    return
+                }
+            } else {
+                print("not a user click")
+                decisionHandler(.allow)
+                return
+            }
+        }
+    
+    /*
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction,
+     withError error: Error, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if (error as NSError).code == -999 {
+           return
+        }
+        print("navigationAction.request.url.... \(String(describing: navigationAction.request.url))" )
+        
+        if navigationAction.navigationType == .linkActivated  {
+            if let url = navigationAction.request.url,
+            let host = url.host,
+                host.hasPrefix("syfpos.com") !=
+                UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url)
+                    decisionHandler(.cancel)
+            } else {
+                // Open in web view
+                decisionHandler(.allow)
+            }
+        } else {
+            // other navigation type, such as reload, back or forward buttons
+            decisionHandler(.allow)
+        }
+    }
+    */
+    /*
     public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
      if (error as NSError).code == -999 {
         return
      }
+        
      print("\(error) unifi error ... " )
-    }
+    } */
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         view.backgroundColor = .systemBackground
         view.addSubview(webView)
         webView.load(URLRequest(url: url))
@@ -110,3 +162,31 @@ class AdvWebviewViewController: UIViewController, WKScriptMessageHandler {
         
     }
 }
+
+/*
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        // Check for links.
+        if navigationAction.navigationType == .linkActivated {
+            // Make sure the URL is set.
+            guard let url = navigationAction.request.url else {
+                decisionHandler(.allow)
+                return
+            }
+
+            // Check for the scheme component.
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            if components?.scheme == "http" || components?.scheme == "https" {
+                // Open the link in the external browser.
+                UIApplication.shared.open(url)
+                // Cancel the decisionHandler because we managed the navigationAction.
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+}
+*/
